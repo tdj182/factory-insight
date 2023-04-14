9990dd4bc45ba60ebe81decf0f903454a5964562
tdj182 <tdj182@gmail.com>
2023-04-14 17:02:24 -0500

    trying Navigate to send to different url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Factories from './components/Factories';
@@ -8,6 +8,16 @@ import Codex from './components/Codex';
 
 
 function App() {
+  const [refreshed, setRefreshed] = useState(false);
+
+  useEffect(() => {
+    const navigationEntries = window.performance.getEntriesByType('navigation');
+    const navigationEntry = navigationEntries[navigationEntries.length - 1];
+    if (navigationEntry.type === 'reload') {
+      setRefreshed(true);
+    }
+  }, []);
+  
   return (
     <div className="App" style={{ backgroundColor: '#525252' }}>
       <Router basename="/factory-insight">
@@ -16,6 +26,7 @@ function App() {
           <Route path="/" element={<Factories />} />
           <Route path="/factories" element={<Factories />} />
           <Route path="/codex" element={<Codex />} />
+          {refreshed && <Route path="*" element={<Navigate to="/" />} />}
         </Routes>
       </Router>
     </div>
